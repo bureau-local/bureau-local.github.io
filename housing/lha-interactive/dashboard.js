e3bd0d81c785c0dda48a9208838e9419201844f2
Charles Boutaud <cboutaud@users.noreply.github.com>
2019-10-02 14:45:16 +0100

    Housing lha interactive
    Housing lha interactive intial commit with placeholder data

---
+++ housing/lha-interactive/dashboard.js
@@ -0,0 +1,60 @@
+const decodeBRMA = (brma) => {
+  brma = decodeURIComponent(brma)
+  return brma.replace(/_/g, ' ')
+}
+
+// load the brma data
+param = this.location.search.substring(1)
+brma = decodeBRMA(param)
+brmaData = summaryData[brma]
+
+// if the data is undefined return to the first page
+if (brmaData == undefined) {
+  this.location.href = "splash.html"
+}
+
+// change the name of BRMA in the interactive
+document.getElementById("brma-name").innerHTML = brma
+
+// change the number of affordable listings in the interactive
+var affordableListings = brmaData["affordable-listings"]
+document.getElementById("affordable-listings").innerHTML = affordableListings
+
+// change the total number of listings in the interactive
+var totalListings = brmaData["total-listings"]
+document.getElementById("total-listings").innerHTML = " / " + totalListings
+
+// add css rule to slide the gradient from 100 to 1 style sheet
+var cssSheetName = "dashboard.css"
+var selector = "link[href$='" + cssSheetName + "']"
+var stylesheet = document.querySelector(selector).sheet
+
+var percent = affordableListings / totalListings.replace(",", "")
+var limit = 100 - Math.floor((percent * 100))
+
+frames = ""
+for (let i = 0; i <= limit; i ++) {
+  colorUpTo = (100 - i)
+  if (i == limit) {
+    colorUpTo = (percent * 100)
+  }
+  gradient = "linear-gradient(to top, #e66 " + colorUpTo + "%, #444 0)"
+  frame = i + "% {background-image: " + gradient + "}"
+  frames += frame
+}
+
+var final_frame = "100% {background-image: " + gradient + "}"
+frames += final_frame
+stylesheet.insertRule("@keyframes dropSlide {" + frames + "}")
+
+// change the second sentence of the vis text
+var monthlyTopUp = "£" + brmaData["monthly-top-up"]
+if (percent < 0.3) {
+  start  = " The average benefit allowance in your area would need to increase by <span style='font-size: 2rem; font-weight: bold; color:#ff5a5d;'>"
+  end = "</span> a month for someone to afford the cheapest 30% of homes."
+  sentence = start + monthlyTopUp + end
+  document.getElementById("second-sentence").innerHTML = sentence
+} else {
+  sentence = " Unlike most areas, you would be able to afford to rent the cheapest 30% of homes with the current benefit allowance in your area."
+  document.getElementById("second-sentence").innerHTML = sentence
+}
---
+++ housing/lha-interactive/splash.js
@@ -0,0 +1,95 @@
+const encodeBRMA = (brma) => {
+  brma = brma.replace(/ /g, '_')
+  return encodeURIComponent(brma)
+}
+
+// navigate to dashboard
+function navigate (brma) {
+  var param = encodeBRMA(brma)
+  this.location.href = "dashboard.html?" + param
+}
+
+// search submit
+async function onSubmit(event) {
+  event.preventDefault()
+  search = document.getElementById("search").value
+  error = document.getElementById("error")
+
+  // show error for invalid postcodes
+  if (!isPostcode(search)) {
+    error.innerHTML = "We couldn\'t find this postcode"
+    return
+  }
+
+  try {
+    sendSearchEvent('postcode-search', search.toUpperCase())
+    search = await lookup(search)
+    navigate(search)
+  } catch (e) {
+    error.innerHTML = "We couldn\'t find this postcode"
+  }
+}
+
+// lookup for brma
+async function lookup (search) {
+  const url = 'https://api.postcodes.io/postcodes/' + encodeURIComponent(search)
+  const response = await window.fetch(url)
+  const json = await response.json()
+
+  const eastings = json && json.result && json.result.eastings
+  if (!eastings) {
+    throw new Error()
+  }
+
+  const northings = json && json.result && json.result.northings
+  if (!northings) {
+    throw new Error()
+  }
+
+  const point = turf.point([eastings, northings])
+  for (var shape in brmaShapes.features) {
+    const brmaGeometry = brmaShapes.features[shape].geometry
+    const geoType = brmaGeometry.type
+    const coordinates = brmaGeometry.coordinates
+
+    var polygon
+    if (geoType == "Polygon") {
+      polygon = turf.polygon(coordinates)
+    } else if (geoType == "MultiPolygon") {
+      polygon = turf.multiPolygon(coordinates)
+    }
+
+    // check if point in polygon
+    if (turf.booleanPointInPolygon(point, polygon)) {
+      const foundName = brmaShapes.features[shape].properties.Name
+      return foundName
+    }
+  }
+
+  if (!foundName) {
+    throw new Error()
+  }
+}
+
+// Postcode validation
+const isPostcode = (value) => {
+  value = value.toLowerCase().trim();
+  const postcodeRegex = /^[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}$/ig
+  const matches = value.match(postcodeRegex)
+  return matches && matches.length
+}
+
+// analytics
+const sendSearchEvent = (type, label) => {
+  window.gtag('event', type, {
+    event_category: 'engagement',
+    event_label: label
+  })
+  window.gtag('event', 'search', {
+    event_category: 'engagement',
+    event_label: label
+  })
+}
+
+const searchForm = document.getElementById("form")
+searchForm.addEventListener("submit", onSubmit)
