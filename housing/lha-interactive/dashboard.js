5c706fa784f23c14709100472018a90adba78b1e
Charles Boutaud <cboutaud@users.noreply.github.com>
2020-01-23 17:54:22 +0000

    Interactive update
    Updated the interactive following the gov jan 2020 announcement that lha would be increase according to cpi

--- housing/lha-interactive/dashboard.js
+++ housing/lha-interactive/dashboard.js
@@ -3,6 +3,11 @@ const decodeBRMA = (brma) => {
   return brma.replace(/_/g, ' ')
 }
 
+// if no hash make the hash #page-1
+if (this.location.hash == false) {
+  this.location.hash = "#page-1"
+}
+
 // load the brma data
 param = this.location.search.substring(1)
 brma = decodeBRMA(param)
@@ -17,7 +22,7 @@ if (brmaData == undefined) {
 document.getElementById("brma-name").innerHTML = brma
 
 // change the number of affordable listings in the interactive
-var affordableListings = brmaData["affordable-listings"]
+var affordableListings = brmaData["affordable-listings-april-2019-lha"]
 document.getElementById("affordable-listings").innerHTML = affordableListings
 
 // change the total number of listings in the interactive
@@ -47,30 +52,76 @@ var final_frame = "100% {background-image: " + gradient + "}"
 frames += final_frame
 stylesheet.insertRule("@keyframes dropSlide {" + frames + "}")
 
-// change the second sentence of the vis text
-var monthlyTopUp = "£" + brmaData["monthly-top-up"]
-if (percent < 0.3) {
-  start  = " The average benefit allowance in your area would need to increase by <span style='font-size: 2rem; font-weight: bold; color:#ef3340;'>"
-  end = "</span> a month for someone to afford the cheapest 30% of homes."
-  sentence = start + monthlyTopUp + end
-  document.getElementById("second-sentence").innerHTML = sentence
-} else {
-  sentence = " Unlike most areas, you would be able to afford to rent any of the cheapest 30% of homes with the current benefit allowance in your area."
-  document.getElementById("second-sentence").innerHTML = sentence
-}
+// change text for first slide second sentence
+var april2020LHAAffordable = brmaData["affordable-listings-april-2020-lha"]
+var newAffordableHomes = april2020LHAAffordable - affordableListings
+var newAffordableHomesPercent = (newAffordableHomes / totalListings.replace(",", ""))
 
+if (newAffordableHomes === 0) {
+  newAffordableHomes = "Despite this, <span class=emphasise>not 1</span> more of those homes would be"
+} else if (newAffordableHomes === 1) {
+  newAffordableHomes = "Despite this, only <span class=emphasise>1</span> more of those homes would be"
+} else if (newAffordableHomesPercent > 0.03) {
+  newAffordableHomes = "This would make <span class=emphasise>" + newAffordableHomes + "</span> of those homes "
+} else {
+  newAffordableHomes = "Despite this, only <span class=emphasise>" + newAffordableHomes + "</span> more of those homes would be"
+}
+document.getElementById("new-affordable-homes").innerHTML = newAffordableHomes
+
+// change text for second slide
+if (percent < 0.3) {
+  monthlyTopUp = brmaData["monthly-top-up-april-2020-lha"]
+  april2019LHA = brmaData["april-2019-lha"]
+  april2020LHA = brmaData["april-2020-lha"]
+  april2019RequiredIncrease = brmaData["monthly-top-up-april-2019-lha"]
+  april2020MonthlyIncrease = Math.round((april2020LHA - april2019LHA) * 52.1429 / 12)
+
+  // first sentence
+  start  = "The allowance in your area is set to rise by "
+  end = "<span class='emphasise'>£" + april2020MonthlyIncrease + "</span>. "
+  firstSentence = start + end
+
+  // second sentence
+  if (april2020MonthlyIncrease < april2019RequiredIncrease) {
+    secondSentence = "It would still need to increase by <span class='emphasise'>£" + Math.round(monthlyTopUp) + "</span> a month for you to afford the cheapest 30% of homes in " + brma + "."
+  } else {
+    // second sentence if the planned lha meets the affordability target
+    secondSentence = "Unlike for most areas, you would be able to afford to \
+                      rent any of the cheapest 30% of homes with the planned \
+                      benefit allowance in " + brma + "."
+  }
+
+  secondSlideText = firstSentence + secondSentence
+
+} else {
+  // text if the current lha meets the affordability target
+  var secondSlideText = "Unlike most areas, you would be able to afford to \
+                         rent any of the cheapest 30% of homes with the \
+                         current benefit allowance in " + brma + ".<br>";
+}
+document.getElementById("second-slide").innerHTML = secondSlideText
+
+// toggle between pages
 function togglePage(){
 
-  var toggle = document.getElementsByClassName("page-toggle");
+  hash = this.location.hash.substring(1)
+  target = parseInt(hash.slice(-1)) + 1
+  if (target > 3) {
+    target = 1
+  }
+  targetPage = "page-" + target
 
+  var toggle = document.getElementsByClassName("page-toggle");
   for(let i = 0; i < toggle.length; i++){
-    if (toggle[i].style.display === "none") {
+    if (toggle[i].classList.contains(targetPage)) {
       toggle[i].style.display = "block";
     } else {
       toggle[i].style.display = "none";
-    }  
+    }
   }
-  //Manually add the brma to the link back, because we can't do it while it's hidden
+
+  // manually add the brma to the link back
+  // because we can't do it while it's hidden
   document.getElementById("brma-link").innerHTML = brma
 }
 
@@ -84,4 +135,3 @@ const sendSearchAgain = (label) => {
 
 const searchAgainButton = document.getElementById("search-again")
 searchAgainButton.addEventListener("click", sendSearchAgain("search-again"))
-
